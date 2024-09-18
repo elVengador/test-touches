ebb6c19c2bb3d5c08dc31d2b5b0b52fa54755973
elVengador <dev.jimynicanor@gmail.com>
2024-09-17 23:15:02 -0500

    chore: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { Touch, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { handleTouch } from "./assets/utils";
 
