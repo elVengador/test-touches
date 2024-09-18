6c2589dfc30d680b083465892834e7b605822559
elVengador <dev.jimynicanor@gmail.com>
2024-09-17 22:55:04 -0500

    add test code

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { Touch, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { handleTouch } from "./assets/utils";
+
+function App() {
+  // const [count, setCount] = useState(0);
+  const [touches, setTouches] = useState<Touch[]>([]);
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Test touches</h1>
+      <div
+        className="card"
+        style={{ border: "solid 4px royalBlue" }}
+        // ref={elementRef}
+        onTouchStart={(e) => {
+          console.log("sss");
+          const newTouches = Array.from(e.changedTouches);
+          setTouches([...newTouches]);
+          handleTouch(newTouches);
+        }}
+      >
+        <h2>{touches.length}</h2>
+        <ul style={{ listStyle: "none", padding: "0px" }}>
+          {touches.map((c, i) => (
+            <li key={i} style={{ borderBottom: "solid 2px royalBlue" }}>
+              <div>
+                <h4>Client:</h4>
+                <p>x: {c.clientX.toFixed(2)}</p>
+                <p>y: {c.clientY.toFixed(2)}</p>
+              </div>
+
+              <div>
+                <h4>Page:</h4>
+                <p>x: {c.pageX.toFixed(2)}</p>
+                <p>y: {c.pageY.toFixed(2)}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/utils.ts
@@ -0,0 +1,48 @@
+import { Touch } from "react";
+
+type PinchRatio = number;
+type Scale = number;
+
+// Calculate the distance between two finger positions
+export const calculatePinchRatio = (touches: Touch[]): PinchRatio => {
+  return Math.hypot(
+    touches[0].clientX - touches[1].clientX,
+    touches[0].clientY - touches[1].clientY
+  );
+};
+
+// Check if an event is a "zoom" event
+export const checkZoomEvent = (touches: Touch[]): boolean => {
+  return (
+    Math.hypot(
+      touches[0].clientX - touches[1].clientX,
+      touches[0].clientY - touches[1].clientY
+    ) > 10
+  );
+};
+
+// Apply a zoom effect to an element
+type ZoomInput = {
+  pinchRatio: PinchRatio;
+  scale: Scale;
+};
+export const applyZoom = ({ pinchRatio, scale }: ZoomInput): void => {
+  // Use this function to update the element's CSS transformation
+  // (For example, using `element.style.transform`or `element.style.scale`)
+};
+
+// Handle touch events and apply a zoom effect
+export const handleTouch = (touches: Touch[]): void => {
+  console.log("touch", { touches });
+  if (touches.length !== 2) return;
+  const pinchRatio = calculatePinchRatio(touches);
+  if (!checkZoomEvent(touches)) return;
+
+  applyZoom({ pinchRatio, scale: 1 });
+
+  // After the user finishes pinching or zooming...
+  setTimeout(() => {
+    // Reset the CSS transformation to its original value
+    applyZoom({ pinchRatio: 1, scale: 1 });
+  }, 10); // Wait for a tiny bit of time before snapping back
+};
