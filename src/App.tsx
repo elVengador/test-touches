d6d75e4673652c66593529d88a0cc5e9f786c806
elVengador <dev.jimynicanor@gmail.com>
2024-09-17 23:07:39 -0500

    chore: increaze touch are

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,21 @@ function App() {
   const [touches, setTouches] = useState<Touch[]>([]);
   const [ratio, setRatio] = useState(-1);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div
+      style={{ border: "solid 4px royalBlue" }}
+      onTouchStart={(e) => {
+        console.log("sss");
+        const newTouches = Array.from(e.touches);
+        setTouches([...newTouches]);
+        const newRatio = handleTouch(newTouches);
+        setRatio(newRatio);
+      }}
+    >
       <h1>Test touches</h1>
       <div
         className="card"
-        style={{ border: "solid 4px royalBlue" }}
+
         // ref={elementRef}
-        onTouchStart={(e) => {
-          console.log("sss");
-          const newTouches = Array.from(e.touches);
-          setTouches([...newTouches]);
-          const newRatio = handleTouch(newTouches);
-          setRatio(newRatio);
-        }}
       >
         <h2>
           {touches.length}, ratio: {ratio}
@@ -58,7 +52,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
